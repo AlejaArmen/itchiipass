f44ec2469890f020b15c69e83378bf3cc42c7c89
Polo-lap <lc15551407@chihuahua2.tecnm.mx>
2023-12-04 18:24:41 -0600

    agregando rama

--- components/LoginButton.jsx
+++ components/LoginButton.jsx
@@ -1,12 +1,47 @@
-import React from "react";
+import React, {useState, useEffect} from "react";
 
-export default function LoginButton({onClick}) {
+export default function LoginButton({ onClick }) {
+  const [isMobile, setIsMobile] = useState(false);
+
+  useEffect(() => {
+    const checkWindowSize = () => {
+      setIsMobile(window.innerWidth <= 768);
+    };
+    checkWindowSize();
+    window.addEventListener('resize', checkWindowSize);
+    return()=>{
+      window.removeEventListener('resize', checkWindowSize);
+    };
+  },[]);
+
   return (
     <>
-      <button className="bg-neutralbtn hover:bg-successbtn text-white font-bold px-4 mr-4 my-8 rounded-3xl text-lg"
-      onClick={onClick}>
-        Iniciar sesión
-      </button>
+      <div className="max-sm:">
+        <button
+          className="bg-neutralbtn hover:bg-successbtn text-white font-bold flex justify-center my-5 mx-4 p-3 rounded-3xl text-lg"
+          onClick={onClick}
+        >
+        {isMobile ?(
+
+          <svg
+            xmlns="http://www.w3.org/2000/svg"
+            fill="none"
+            viewBox="0 0 24 24"
+            strokeWidth={1.5}
+            className="roun w-8 h-8 stroke-primary bg-transparent"
+          >
+            <path
+              strokeLinecap="round"
+              strokeLinejoin="round"
+              d="M15.75 6a3.75 3.75 0 11-7.5 0 3.75 3.75 0 017.5 0zM4.501 20.118a7.5 7.5 0 0114.998 0A17.933 17.933 0 0112 21.75c-2.676 0-5.216-.584-7.499-1.632z"
+            />
+          </svg>
+        ) : (
+          "Inicio de sesión"
+        )
+        }
+        </button>
+      </div>
     </>
   );
 }
